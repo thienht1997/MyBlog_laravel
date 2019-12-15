da6ae7fa6cff953f8ac09c7d0bafee7f45ca93dd
Admin <thienht1997tt@gmail.com>
2019-12-16 00:28:31 +0700

    fix migrate

--- database/migrations/2019_08_02_143921_create_category_table.php
+++ database/migrations/2019_08_02_143921_create_category_table.php
@@ -14,7 +14,7 @@ class CreateCategoryTable extends Migration
     public function up()
     {
         Schema::create('category', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id')->autoIncrement();
             $table->string('name');
             $table->timestamps();
         });
